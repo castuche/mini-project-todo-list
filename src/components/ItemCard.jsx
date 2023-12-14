d877795f2803d7627e0495ebd6eb697bbe46ed4d
Catherine Fournier <fourniercath@hotmail.com>
2023-12-14 12:43:25 +0100

    fixed delete button

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,55 +1,18 @@
 import React from 'react';
 import classes from '../styles/ItemCard.module.css';
-import { v4 as uuidv4 } from 'uuid';
 
-function ItemCard({ items, handleDelete }) {
-    const updatedItems = items.map(item => ({ ...item, id: item.id || uuidv4() }));
-
-    return (
-        <ul>
-            {updatedItems.map(currentItem => (
-                <div className={classes.card} key={currentItem.id}>
-                    <h2>{currentItem.task}</h2>
-                    {currentItem.completed ? (
-                        <label className={classes.labelDone}>Done</label>
-                    ) : (
-                        <label className={classes.labelNotDone}>Not done</label>
-                    )}
-                    <button
-                        className={classes.deleteButton}
-                        type="button"
-                        onClick={() => handleDelete(currentItem.id)}
-                    >
-                        Delete this task
-                    </button>
-                </div>
-            ))}
-        </ul>
-    );
-}
-
-export default ItemCard;
-
-
-
-
-/* import classes from "../styles/ItemCard.module.css"
-import { v4 as uuidv4 } from 'uuid';
-
-function ItemCard(items,handleDelete){
-    items = items.map(item => ({ ...item, id: uuidv4() }));
+function ItemCard({items,handleDelete}){
     return(
 <ul>
      {items.map(currentItem => (
-            {currentItem.id=uuidv4()}
             <div className={classes.card} key='currentItem.id'> 
             <h2>{currentItem.task}</h2>
             {currentItem.completed? <label className={classes.labelDone}>Done</label> : <label className={classes.labelNotDone}>Not done</label>}
             <button className={classes.deleteButton} type='button' onClick={()=>handleDelete(currentItem.id)}>Delete this task</button>
             </div>
             ))}
-    </ul>
+</ul>
     )
      }
 
-export default ItemCard; */
+export default ItemCard;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,28 +1,7 @@
-import React, { useState } from 'react';
-import itemsList from '../data/items.json';
-import classes from '../styles/ItemsList.module.css';
-import ItemCard from './ItemCard.jsx';
-
-function ItemList() {
-    const [items, setItems] = useState(itemsList);
-
-    const handleDelete = idToDelete => {
-        const filteredList = items.filter(currentItem => currentItem.id !== idToDelete);
-        setItems(filteredList);
-    };
-
-    return <ItemCard items={items} handleDelete={handleDelete} />;
-}
-
-export default ItemList;
-
-
-
-/* import itemsList from "../data/items.json";
 import classes from "../styles/ItemsList.module.css"
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
 import ItemCard from "./ItemCard.jsx";
+import itemsList from '../data/items.json'
 
 function ItemList(){
     const [items, setItems] = useState(itemsList);
@@ -33,11 +12,11 @@ function ItemList(){
     }
 
     return (
-    <ItemCard(items,handleDelete)/>
+    <ItemCard items={items} handleDelete={handleDelete}/>
     );
      }
   
 
-export default ItemList ; */
+export default ItemList ; 
 
 
