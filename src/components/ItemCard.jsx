5aaedb9e55aa278daaeddf98d2c74876332fe2b4
Catherine Fournier <fourniercath@hotmail.com>
2023-12-14 16:07:34 +0100

    day3 almost done, except bug when clicking cards

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -6,13 +6,13 @@ function ItemCard({items,handleDelete}){
     return(
 <ul>
      {items.map(currentItem => (
-            <Link key={currentItem.id} to={`/detail/${currentItem.id}`}>
             <div className={classes.card}> 
+            <Link key={currentItem.id} to={`/detail/${currentItem.id}`}>
             <h2>{currentItem.task}</h2>
             {currentItem.completed? <label className={classes.labelDone}>Done</label> : <label className={classes.labelNotDone}>Not done</label>}
+            </Link>
             <button className={classes.deleteButton} type='button' onClick={()=>handleDelete(currentItem.id)}>Delete this task</button>
             </div>
-            </Link>
             ))}
 </ul>
     )
