d35452c7a0fd4446657fdaea61bfb548d19e1864
Viviane Huang <vivianehuang2022@gmail.com>
2023-12-15 02:39:25 +0100

    add function added

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,21 +1,32 @@
+// ItemCard.jsx
 import React from 'react';
 import classes from '../styles/ItemCard.module.css';
 import { Link } from 'react-router-dom';
 
-function ItemCard({items,handleDelete}){
-    return(
-<ul>
-     {items.map(currentItem => (
-            <div className={classes.card}> 
-            <Link key={currentItem.id} to={`/detail/${currentItem.id}`}>
+function ItemCard({ items, handleDelete }) {
+  return (
+    <ul>
+      {items.map((currentItem) => (
+        <div className={classes.card} key={currentItem.id}>
+          <Link to={`/detail/${currentItem.id}`}>
             <h2>{currentItem.task}</h2>
-            {currentItem.completed? <label className={classes.labelDone}>Done</label> : <label className={classes.labelNotDone}>Not done</label>}
-            </Link>
-            <button className={classes.deleteButton} type='button' onClick={()=>handleDelete(currentItem.id)}>Delete this task</button>
-            </div>
-            ))}
-</ul>
-    )
-     }
+            {currentItem.completed ? (
+              <label className={classes.labelDone}>Done</label>
+            ) : (
+              <label className={classes.labelNotDone}>Not done</label>
+            )}
+          </Link>
+          <button
+            className={classes.deleteButton}
+            type="button"
+            onClick={() => handleDelete(currentItem.id)}
+          >
+            Delete this task
+          </button>
+        </div>
+      ))}
+    </ul>
+  );
+}
 
 export default ItemCard;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,22 +1,93 @@
-import classes from "../styles/ItemsList.module.css"
+// ItemList.jsx
+import classes from "../styles/ItemsList.module.css";
 import { useState } from "react";
 import ItemCard from "./ItemCard.jsx";
-import itemsList from '../data/items.json'
+import itemsList from "../data/items.json";
 
-function ItemList(){
-    const [items, setItems] = useState(itemsList);
+function ItemList() {
+  const [items, setItems] = useState(itemsList);
+  const [editingItemId, setEditingItemId] = useState(null);
+  const [selectedTasks, setSelectedTasks] = useState([]);
 
-    const handleDelete = idToDelete => {
-        const filteredList = items.filter(currentItem => currentItem.id !== idToDelete)
-        setItems(filteredList)
-    }
-
-    return (
-    <ItemCard items={items} handleDelete={handleDelete}/>
+  const handleToggleComplete = (id) => {
+    const updatedItems = items.map((item) =>
+      item.id === id ? { ...item, completed: !item.completed } : item
     );
-     }
-  
+    setItems(updatedItems);
+    setEditingItemId(null);
+  };
 
-export default ItemList ; 
+  const handleToggleEdit = (id) => {
+    setEditingItemId((prevId) => (prevId === id ? null : id));
+  };
 
 
+  const handleEdit = (id, newTask) => {
+    const updatedItems = items.map((item) =>
+      item.id === id ? { ...item, task: newTask } : item
+    );
+    setItems(updatedItems);
+    setEditingItemId(null);
+  };
+
+
+  const handleCheckboxChange = (id) => {
+    if (selectedTasks.includes(id)) {
+      setSelectedTasks(selectedTasks.filter((taskId) => taskId !== id));
+    } else {
+      setSelectedTasks([...selectedTasks, id]);
+    }
+  };
+
+  const handleDeleteSelected = () => {
+    const updatedItems = items.filter(
+      (item) => !selectedTasks.includes(item.id)
+    );
+    setItems(updatedItems);
+    setSelectedTasks([]);
+  };
+
+  const handleAddTask = (completed) => {
+    const newTask = {
+      id: items.length + 1,
+      task: `New Task ${items.length + 1}`,
+      completed: completed,
+    };
+
+    setItems([...items, newTask]);
+  };
+
+  return (
+    <div>
+      <h2>Not Completed Tasks</h2>
+      <ItemCard
+        items={items.filter((item) => !item.completed)}
+        handleToggleComplete={handleToggleComplete}
+        handleToggleEdit={handleToggleEdit}
+        handleEdit={handleEdit}
+        handleDelete={(id) => setItems(items.filter((item) => item.id !== id))}
+        handleCheckboxChange={handleCheckboxChange}
+        selectedTasks={selectedTasks}
+      />
+      <button onClick={() => handleAddTask(false)}>Add New Task</button>
+
+      <h2>Completed Tasks</h2>
+      <ItemCard
+        items={items.filter((item) => item.completed)}
+        handleToggleComplete={handleToggleComplete}
+        handleToggleEdit={handleToggleEdit}
+        handleEdit={handleEdit}
+        handleDelete={(id) => setItems(items.filter((item) => item.id !== id))}
+        handleCheckboxChange={handleCheckboxChange}
+        selectedTasks={selectedTasks}
+      />
+      <button onClick={() => handleAddTask(true)}>Add New Task</button>
+
+      {selectedTasks.length > 0 && (
+        <button onClick={handleDeleteSelected}>Delete Selected</button>
+      )}
+    </div>
+  );
+}
+
+export default ItemList;
